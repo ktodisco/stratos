14517c49ea2adda8670ca5b340a4ee9aaf161ae3
ktodisco <kevintodisco@gmail.com>
2020-07-16 23:26:59 -0400

    More on descriptor sets.
    Setup for the API change to fill descriptor sets in one API call on resource tables, then set up binding calls.

--- src/engine/graphics/platform/vulkan/st_vk_resource_table.cpp
+++ src/engine/graphics/platform/vulkan/st_vk_resource_table.cpp
@@ -8,6 +8,26 @@
 
 #if defined(ST_GRAPHICS_API_VULKAN)
 
+#include <graphics/platform/vulkan/st_vk_render_context.h>
 
+st_vk_resource_table::st_vk_resource_table()
+{
+	st_vk_render_context* context = st_vk_render_context::get();
+
+	context->create_descriptor_set(st_descriptor_slot_textures, &_textures);
+	context->create_descriptor_set(st_descriptor_slot_buffers, &_buffers);
+	context->create_descriptor_set(st_descriptor_slot_constants, &_constants);
+	context->create_descriptor_set(st_descriptor_slot_samplers, &_samplers);
+}
+
+st_vk_resource_table::~st_vk_resource_table()
+{
+	st_vk_render_context* context = st_vk_render_context::get();
+
+	context->destroy_descriptor_set(_textures);
+	context->destroy_descriptor_set(_buffers);
+	context->destroy_descriptor_set(_constants);
+	context->destroy_descriptor_set(_samplers);
+}
 
 #endif
--- src/engine/graphics/platform/vulkan/st_vk_resource_table.h
+++ src/engine/graphics/platform/vulkan/st_vk_resource_table.h
@@ -15,19 +15,11 @@
 #include <cstdint>
 #include <vector>
 
-// For descriptors, I need:
-// a layout.  the layout is determined by the order and binding locations of the resources
-//	this is actually most akin to the root signature definition in the dx12 layer
-//	could define these statically
-// a descriptor pool to allocate descriptor sets from
-// a descriptor set created using the corresponding layout and allocated from the pool
-// a call to copy or write descriptors to the set
-
 class st_vk_resource_table
 {
 public:
-	st_vk_resource_table() {}
-	~st_vk_resource_table() {}
+	st_vk_resource_table();
+	~st_vk_resource_table();
 
 	void add_constant_buffer(class st_vk_constant_buffer* cb) {}
 	void add_texture_resource(class st_vk_texture* sr) {}
@@ -36,10 +28,10 @@ public:
 	void bind(class st_vk_render_context* context) {}
 
 private:
-	std::vector<st_vk_descriptor> _cbvs;
-	std::vector<st_vk_descriptor> _srvs;
-	std::vector<st_vk_descriptor> _samplers;
-	std::vector<st_vk_descriptor> _buffers;
+	vk::DescriptorSet _textures;
+	vk::DescriptorSet _constants;
+	vk::DescriptorSet _buffers;
+	vk::DescriptorSet _samplers;
 };
 
 #endif
